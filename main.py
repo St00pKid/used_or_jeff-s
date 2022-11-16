2780b974f2b77f7c20d8fb57f6543ffebc96afe4
St00pKid <zombiferproductions@gmail.com>
2022-11-16 17:34:09 -0500

    Adding new lists for used dir.

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import shutil
 import time
 
 
-FOLDER_LIST = []
 
 with open('used_records.json', 'r') as x:
     used_dict = json.load(x)
 
 def sorter():
-    for item in FOLDER_LIST:
+    sort_dir = []
+    used_dir = []
+    cfl.create_folder_list('sort_me', sort_dir)
+    print(sort_dir)
+    for item in sort_dir:
         if item not in used_dict:
             dir_util.copy_tree(f"sort_me/{item}",
                                     f"tobeposted/{item}")
@@ -21,13 +24,20 @@ def sorter():
                                     f"used/{item}")
 
         shutil.rmtree(f'sort_me/{item}/')
+    
+    cfl.create_folder_list('used', used_dir)
+    for item in used_dir:
+        shutil.move(item, 'used') # TODO: this is broken at the moment.
+        
 
+###########################
 start = time.perf_counter()
 
-cfl.create_folder_list(FOLDER_LIST)
-cfl.csv_to_json()
+# cfl.csv_to_json()
 sorter()
 
+
 finish = time.perf_counter()
+############################
 
 print(f"completed in {finish - start:0.4f} seconds")
