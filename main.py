0bd8cb53251abf6ca934ebfbdb199d1704fb2b89
St00pKid <zombiferproductions@gmail.com>
2022-11-23 16:35:34 -0500

    Moved folders to ebay drive for testing.

--- main.py
+++ main.py
@@ -5,33 +5,32 @@ import shutil
 import time
 
 
-
 with open('used_records.json', 'r') as x:
     used_dict = json.load(x)
 
 def sorter():
     sort_dir = []
     used_dir = []
-    cfl.create_folder_list('sort_me', sort_dir)
+    cfl.create_folder_list('/Volumes/ebay/sort_me', sort_dir)
     for item in sort_dir:
         if item not in used_dict:
-            dir_util.copy_tree(f"sort_me/{item}",
-                                    f"tobeposted/{item}")
+            dir_util.copy_tree(f"/Volumes/ebay/sort_me/{item}",
+                                    f"/Volumes/ebay/testtobeposted/{item}")
         
         elif used_dict[f'{item}']['ebay_id'] == '' or used_dict[f'{item}']['ebay_status'] == "Shipped":
-            dir_util.copy_tree(f"sort_me/{item}",
-                                    f"used/{item}")
+            dir_util.copy_tree(f"/Volumes/ebay/sort_me/{item}",
+                                    f"/Volumes/ebay/testused/{item}")
         else:
             print(f'oops! {item} is throwing an error.')
-        shutil.rmtree(f'sort_me/{item}/')
+        shutil.rmtree(f'/Volumes/ebay/sort_me/{item}/')
     
-    cfl.create_folder_list('used', used_dir)
+    cfl.create_folder_list('/Volumes/ebay/testused', used_dir)
     for item in used_dir:
         sub_folder = []
-        cfl.create_folder_list(f'used/{item}', sub_folder)
+        cfl.create_folder_list(f'/Volumes/ebay/testused/{item}', sub_folder)
         for photo in sub_folder:
-            shutil.move(f'used/{item}/{photo}', 'used')
-        shutil.rmtree(f'used/{item}')
+            shutil.move(f'/Volumes/ebay/testused/{item}/{photo}', 'used')
+        shutil.rmtree(f'/Volumes/ebay/testused/{item}')
 
 ###########################
 start = time.perf_counter()
