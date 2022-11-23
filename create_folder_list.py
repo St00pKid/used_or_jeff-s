c38a28bdb51af8510abdcece26a01d0b509b9b66
St00pKid <zombiferproductions@gmail.com>
2022-11-23 14:25:56 -0500

    Added arguements to create_new_list and csv_to_jso

--- create_folder_list.py
+++ create_folder_list.py
@@ -1,7 +1,6 @@
 from os import listdir
 from csv import DictReader, reader, writer
-from json import dumps
-
+import json
 
 def create_folder_list(src_location , folder_list):
     """Create folder list from condition folders. 
@@ -20,10 +19,9 @@ def csv_to_json(src_file):
     # Open and write ebay module csv file to a dictionary list
     json_array = []
     with open(f'{src_file}', 'r') as ref, open('used_records.json', 'r') as used_records:
-        loaded_json = used_records.load()
         data_list = DictReader(ref)
         for row in data_list:
-            if row not in loaded_json.keys():
+            if row not in used_records:
                 json_array.append(row)
                 
     # Loop through list and write the itemID value to key for each object and set value to object to create a dictionary of dictionaries
@@ -32,17 +30,20 @@ def csv_to_json(src_file):
         itemid = i['ItemID']
         formatted_dict[itemid] = i
     
-    # Write the formatted dictionary to json file.
-    with open('used_records.json', 'a') as jsonfile:
-        json_string = dumps(formatted_dict,indent=4)
+    # Write the formatted dictionary to json file. Overwrites existing file. Overwriting removes the need to purge old records.
+    with open('used_records.json', 'w') as jsonfile:
+        json_string = json.dumps(formatted_dict,indent=4)
         jsonfile.write(json_string)
 
-def create_new_list():
-    """Reads FileMaker export CSV file and appends entries that are not empty to new list. Writes to new csv called trimmed_file.csv"""
+def create_new_list(src_file, trimmed_file):
+    """Reads FileMaker export CSV file and appends entries that are not empty to new list. Writes to new csv called trimmed_file.csv
+    First arguement needs to be a csv from filemaker to be formatted correctly. Second arg is the trimmed csv file (usually 'trimmed_file.csv) and will be formatted to json.
+    Header is assumed to be 'ItemID', 'ebay_id', 'instock_qty', 'warehouse_qty', 'ebay_team_qty', 'newitemreceiveddate', 
+             'last_upload_date', 'ebay_status', 'last_markedforebay'"""
     temp_list = []
     new_list = []
 
-    with open("Untitled.csv", 'r', newline='') as csv_file:
+    with open(f'{src_file}', 'r', newline='') as csv_file:
         csv_list = list(reader(csv_file))
         for row in csv_list:
             if not row[0] == '':
@@ -55,15 +56,12 @@ def create_new_list():
     HEADER_FM = ['ItemID', 'ebay_id', 'instock_qty', 'warehouse_qty', 'ebay_team_qty', 'newitemreceiveddate', 
              'last_upload_date', 'ebay_status', 'last_markedforebay']
     
-    with open('trimmed_file.csv', 'w') as trimmed_file:
-        csv_writer = writer(trimmed_file)
+    with open(f'{trimmed_file}', 'w') as tf:
+        csv_writer = writer(tf)
         csv_writer.writerow(HEADER_FM)
         csv_writer.writerows(new_list)
-
+        
+    csv_to_json(f'{trimmed_file}')
    # TODO: the filemaker all used list has a blank entry in the second[1] column for items that are not in the module.
    # This provides a method to sort used items between if the image files need to go to imagetools vs tobeposted. 
-   # In its current form the create_new_list function will trim the FM file to a more readable form.
-   # File can be run through the csv_to_json to format the data into a more readable form. 
-
-create_new_list()
-csv_to_json('trimmed_file.csv')
+   # This file is ready, sort in main needs changed to look for the proper values in used_records.json.
