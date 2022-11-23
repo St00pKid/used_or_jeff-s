fdb6d0a6d311ef4cecd9c3e712d815d0025f5bd6
St00pKid <zombiferproductions@gmail.com>
2022-11-23 16:56:00 -0500

    Changed paths in sorter func to arguments.

--- main.py
+++ main.py
@@ -8,35 +8,35 @@ import time
 with open('used_records.json', 'r') as x:
     used_dict = json.load(x)
 
-def sorter():
+def sorter(sort_me, tobeposted, used):
     sort_dir = []
     used_dir = []
-    cfl.create_folder_list('/Volumes/ebay/sort_me', sort_dir)
+    cfl.create_folder_list(sort_me, sort_dir)
     for item in sort_dir:
         if item not in used_dict:
-            dir_util.copy_tree(f"/Volumes/ebay/sort_me/{item}",
-                                    f"/Volumes/ebay/testtobeposted/{item}")
+            dir_util.copy_tree(f"{sort_me}/{item}",
+                                    f"{tobeposted}/{item}")
         
         elif used_dict[f'{item}']['ebay_id'] == '' or used_dict[f'{item}']['ebay_status'] == "Shipped":
-            dir_util.copy_tree(f"/Volumes/ebay/sort_me/{item}",
-                                    f"/Volumes/ebay/testused/{item}")
+            dir_util.copy_tree(f"{sort_me}/{item}",
+                                    f"{used}/{item}")
         else:
-            print(f'oops! {item} is throwing an error.')
-        shutil.rmtree(f'/Volumes/ebay/sort_me/{item}/')
+            print(f'oops! {item} is wrong.')
+        shutil.rmtree(f'{sort_me}/{item}/')
     
-    cfl.create_folder_list('/Volumes/ebay/testused', used_dir)
+    cfl.create_folder_list(used, used_dir)
     for item in used_dir:
         sub_folder = []
-        cfl.create_folder_list(f'/Volumes/ebay/testused/{item}', sub_folder)
+        cfl.create_folder_list(f'{used}/{item}', sub_folder)
         for photo in sub_folder:
-            shutil.move(f'/Volumes/ebay/testused/{item}/{photo}', 'used')
-        shutil.rmtree(f'/Volumes/ebay/testused/{item}')
+            shutil.move(f'{used}/{item}/{photo}', f'{used}')
+        shutil.rmtree(f'{used}/{item}')
 
 ###########################
 start = time.perf_counter()
 
 cfl.create_new_list('Untitled.csv', 'trimmed_file.csv')
-sorter()
+sorter('/Volumes/ebay/sort_me', '/Volumes/ebay/testtobeposted', '/Volumes/ebay/testused' )
 
 finish = time.perf_counter()
 ############################
